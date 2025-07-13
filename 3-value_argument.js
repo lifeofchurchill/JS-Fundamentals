005f3f800c080ef57f6ea553befc8eb95262c930
Churchill <reghoyah@gmail.com>
2025-07-13 00:11:56 +0000

    updated js code

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,10 +1,2 @@
-const firstArg = 35;
- if (firstArg < 5) {
-    console.log("No argument")
- }
- else if (firstArg > 5 && firstArg < 25) {
-    console.log("HBTN")
-}
-else {
-    console.log("HBTN Cool")
-}
+const args = process.argv.slice(2);
+console.log(args[0] !== undefined ? args[0] : 'No argument');
